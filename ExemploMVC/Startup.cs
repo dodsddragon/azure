3e3c957c2e4981a539cbfd12292a549711379192
dodsddragon <dhouglas.lb@hotmail.com>
2014-03-29 15:33:42 -0300

    Upload Inicial

---
+++ ExemploMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExemploMVC.Startup))]
+namespace ExemploMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
